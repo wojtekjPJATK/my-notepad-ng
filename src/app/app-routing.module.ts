ca038ef3b5a76a0cd8d7749fb64ebbbc04de8f63
Wojtek <wojtekj1989@gmail.com>
2019-01-28 00:09:29 +0100

    first and last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NoteListComponent } from "./components/note-list/note-list.component";
+import { AboutComponent } from "./components/pages/about/about.component";
+import { QuoteComponent } from "./components/pages/quote/quote.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: NoteListComponent },
+  { path: "about", component: AboutComponent },
+  { path: "quote", component: QuoteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ClarityModule } from "@clr/angular";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { NoteListComponent } from "./components/note-list/note-list.component";
+import { NoteItemComponent } from "./components/note-item/note-item.component";
+import { AboutComponent } from "./components/pages/about/about.component";
+import { AddNoteComponent } from "./components/add-note/add-note.component";
+import { FormQuestionComponent } from "./components/form-question/form-question.component";
+import { QuestionControlService } from "./services/question-control.service";
+import { QuestionService } from "./services/question.service";
+import { QuoteComponent } from "./components/pages/quote/quote.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NoteListComponent,
+    NoteItemComponent,
+    AboutComponent,
+    AddNoteComponent,
+    FormQuestionComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [QuestionControlService, QuestionService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
